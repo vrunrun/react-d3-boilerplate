594968adb4df0f25d67a50b7f7d49fd2f48dd5ff
kl <kl@debian>
2020-01-03 10:38:28 +0800

    Build rows for table

--- src/Table.js
+++ src/Table.js
@@ -2,28 +2,56 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import { FormControl } from "react-bootstrap";
 
 export default class Table extends Component {
+  renderRows() {
+    return this.props.data.map(student => {
+      return (
+        <Row key={student.name} style={{ marginTop: "10px" }}>
+          <Col xs={3}>{student.name}</Col>
+          <Col xs={3}>{student.height}</Col>
+          <Col xs={3}>{student.age}</Col>
+          <Col xs={3}>
+            <Button
+              variant={"danger"}
+              type={"button"}
+              style={{ width: "100%" }}
+              name={student.name}
+            >
+              Remove
+            </Button>
+          </Col>
+        </Row>
+      );
+    });
+  }
+
   render() {
     return (
-      <Row>
-        <Col xs={3}>
-          <FormControl placeholder={"Name"} name={"name"} />
-        </Col>
-        <Col xs={3}>
-          <FormControl placeholder={"Height"} name={"height"} />
-        </Col>
-        <Col xs={3}>
-          <FormControl placeholder={"Age"} name={"age"} />
-        </Col>
-        <Col>
-          <Button variant={"primary"} type={"button"} style={{ width: "100%" }}>
-            Add
-          </Button>
-        </Col>
-      </Row>
+      <div>
+        <Row>
+          <Col xs={3}>
+            <FormControl placeholder={"Name"} name={"name"} />
+          </Col>
+          <Col xs={3}>
+            <FormControl placeholder={"Height"} name={"height"} />
+          </Col>
+          <Col xs={3}>
+            <FormControl placeholder={"Age"} name={"age"} />
+          </Col>
+          <Col>
+            <Button
+              variant={"primary"}
+              type={"button"}
+              style={{ width: "100%" }}
+            >
+              Add
+            </Button>
+          </Col>
+        </Row>
+        {this.renderRows()}
+      </div>
     );
   }
 }
